45fb48984bc75623aca5b246c0a6027ade617404
ShionKoga <shion_koga@mail.toyota.co.jp>
2022-04-26 22:40:55 +0900

    githubUsersRepoを呼び出していることのテストを追加

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -3,27 +3,36 @@ import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import {act} from "react-dom/test-utils"
 import Home from "../Home"
-import StubGithubUsersRepo from "./StubGithubUsersRepo";
+import SpyStubGithubUsersRepo from "./SpyStubGithubUsersRepo";
 
 describe('home', () => {
-    let stubGithubUsersRepo: StubGithubUsersRepo
+    let spyStubGithubUsersRepo: SpyStubGithubUsersRepo
 
     beforeEach(() => {
-        stubGithubUsersRepo = new StubGithubUsersRepo()
+        spyStubGithubUsersRepo = new SpyStubGithubUsersRepo()
     })
 
     test('GitHubのユーザー名の一覧が表示される', async () => {
-        stubGithubUsersRepo.getUsers_returnValue = Promise.resolve([
+        spyStubGithubUsersRepo.getUsers_returnValue = Promise.resolve([
             {userName: 'userName1'},
             {userName: 'userName2'},
         ])
 
         // eslint-disable-next-line testing-library/no-unnecessary-act
         await act(async () => {
-            await render(<Home githubUsersRepo={stubGithubUsersRepo}/>)
+            await render(<Home githubUsersRepo={spyStubGithubUsersRepo}/>)
         })
 
         expect(screen.getByText('userName1')).toBeInTheDocument()
         expect(screen.getByText('userName2')).toBeInTheDocument()
+    })
+
+    test('githubUsersRepoを呼んでいる', async () => {
+        // eslint-disable-next-line testing-library/no-unnecessary-act
+        await act(async () => {
+            await render(<Home githubUsersRepo={spyStubGithubUsersRepo}/>)
+        })
+
+        expect(spyStubGithubUsersRepo.getUsers_wasCalled).toBeTruthy()
     })
 })
--- src/tests/SpyStubGithubUsersRepo.ts
+++ src/tests/SpyStubGithubUsersRepo.ts
@@ -1,10 +1,12 @@
 import GithubUsersRepo from "../GithubUsersRepo";
 import GithubUser from "../GithubUser";
 
-export default class StubGithubUsersRepo implements GithubUsersRepo {
+export default class SpyStubGithubUsersRepo implements GithubUsersRepo {
     getUsers_returnValue: Promise<GithubUser[]> = Promise.resolve([])
+    getUsers_wasCalled: boolean = false
 
     getUsers(): Promise<GithubUser[]> {
+        this.getUsers_wasCalled = true
         return this.getUsers_returnValue
     }
 }
